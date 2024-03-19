e15929a7ca31b2cc02e7e08eaf092ce27d6250a9
Kadir Gün <kadir@kadirgun.com>
2024-03-19 14:15:13 +0300

    Add svn remove function

---
+++ src/svn/remove.ts
@@ -0,0 +1,29 @@
+import { getExecOutput } from '@actions/exec'
+
+export type RemoveParams = {
+  force?: boolean
+}
+
+export default async (path: string, params?: RemoveParams) => {
+  const args = ['remove']
+
+  params = params || {}
+
+  if (params.force) {
+    args.push('--force')
+  }
+
+  args.push(path)
+
+  const output = await getExecOutput('svn', args)
+
+  if (output.exitCode !== 0) {
+    throw new Error(output.stderr)
+  }
+
+  const statusRegex = /^[A-Z]\s+(.*)$/
+
+  return output.stdout.split('\n').filter(line => {
+    return statusRegex.test(line)
+  })
+}
